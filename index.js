1323a334691a6aa2fff417e4bfb26ec7e8c59b83
moein-zx <moeinrealmadrid3@gmail.com>
2026-08-19 01:11:37 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // picture changing is start 
 {
     const sources = [
-        {src : "image1.jpg"},
-        {src : "image2.jpg"},
-        {src : "image3.jpg"},
-        {src : "image4.jpg"}
+        {src : "image1.png"},
+        {src : "image2.png"},
+        {src : "image3.png"},
+        {src : "image4.png"}
     ];
 
     const picturediv = document.querySelector(".photoimagecontainer");
@@ -117,14 +117,114 @@
         }
 
         _okay(){
-            alert("مرسی که منو انتخاب کردی ! بوس بهت");
+            alert('بهترین انتخاب عمرت رو کردی !!! بوس بهت .......')
         }
 
         _noo(){
-            alert("دلت میاد دل این پسر خوشگل رو بشکونی ؟؟ با من باش");
+            alert('دلت میاد پسر به این خوشگلی رو رد بکنی؟؟ بهم یه فرصت بده')
         }
     }
 
     new hearttalke(hearttalkbuttondiv)
 }
 // hearttalk button is finished 
+
+
+// firstpage gofl is start 
+{   
+    const newtag = document.createElement('h4');
+    newtag.textContent = 'قربون خانومی برم !!!!';
+    newtag.classList.add('sefareshi');
+
+
+
+    const changerang = document.querySelector('.firsttime');
+    const goflbox = document.querySelector(".firsttime-content");
+    const okaygofl = document.querySelector(".romantic");
+
+    class gofli{
+        constructor(goflbox , okaygofl  , newtag , changerang){
+            this._goflbox = goflbox;
+            this._okaygofl = okaygofl;
+            this._newtag = newtag;
+            this._changerang = changerang;
+            this._onlock = false;
+
+            this._goflbox.addEventListener('click' , (e) => {
+                const {target} = e;
+                const y = target.dataset.action;
+                if(y && `_${y}` in this){
+                    this[`_${y}`](target);
+                }
+            } );  
+        }
+
+        _gofl(target){
+            if(this._onlock){
+                return;
+            }
+            const x = prompt("Please Enter your password to continue ... ");
+
+            if(x === "moein"){
+                alert("your password is correct");
+                this._okaygofl.style.display = 'block';
+                this._goflbox.append(this._newtag);
+                this._changerang.style.background = 'linear-gradient(135deg, #3a1524, #1b0b12)';
+                this._onlock = true;
+            }else{
+                alert("your password is not correct");
+            }
+        }
+    }
+
+    new gofli(goflbox , okaygofl , newtag , changerang);
+}
+// firstpage gofl is finished 
+
+
+// my option button is start 
+{
+    const optionbox = document.querySelector('.myoption_container');
+    const opnbutton = document.querySelector('.optionbutton');
+    const newoptionboxx = document.querySelector('.newtagbox');
+
+    const newoption = document.createElement('p');
+    newoption.textContent = " جیگر بابایی کیه ؟؟";
+    newoption.classList.add('newoption');
+
+    class optionn{
+        constructor(optionbox , opnbutton , newoption , newoptionboxx){
+            this._optionbox = optionbox;
+            this._opnbutton = opnbutton;
+            this._newoption = newoption;
+            this._newoptionboxx = newoptionboxx;
+
+            this._optionbox.addEventListener('click' , (e) =>{
+                const {target} = e;
+
+                const action = target.dataset.action;
+
+                if(action && `_${action}` in this){
+                    this[`_${action}`]();
+                }
+            })
+        }
+
+        _option(){
+            if(this._newoptionboxx.contains(this._newoption)){
+                this._newoption.style.display = 'block';
+                setTimeout(()=>{
+                    this._newoption.style.display = 'none';
+                },2000)
+            }else{
+                this._newoptionboxx.append(this._newoption);
+                setTimeout(()=>{
+                    this._newoption.style.display = 'none';
+                },2000)
+            }
+        }
+    }
+
+    new optionn(optionbox , opnbutton , newoption , newoptionboxx);
+}
+// my option button is finished 
